2c6e4ab05f44c4973f59b4a35e6a3e38551dc8af
Ray Riffy <rayriffy@gmail.com>
2017-06-25 12:27:37 +0700

    Javascript setup

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.modal').modal({
+      startingTop: '4%',
+      endingTop: '10%'
+    });
+    Materialize.toast('WARNING: This is an unstable releases! Please keep in mind.', 500000);
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
